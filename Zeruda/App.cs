d8ee7d8ce84fb16547da120f4a16451621b68a57
Efthimia Aivaloglou <e.aivaloglou@tudelft.nl>
2015-10-27 13:16:52 +0100

    Moved Zeruda and tests to their own repository

---
+++ Zeruda/App.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infotron.Parsing;
+
+namespace Zeruda
+{
+    public class LinkAnalysis
+    {
+
+        public static int NumberOfParameters(string formula)
+        {
+            var fa = new FormulaAnalyzerExtension(formula);
+            return fa.NParameters();
+        }
+
+        public static bool TypeofFinalParameter(string formula)
+        {
+            bool Has4 = LinkAnalysis.NumberOfParameters(formula) == 4;
+
+            if (Has4)
+            {
+                var fa = new FormulaAnalyzerExtension(formula);
+                return fa.TypeOfFinal();
+            }
+            else
+            {
+                throw new InvalidOperationException(formula + " does not have 4 parameters");
+            }
+
+        }
+
+        public static List<string> ReturnLookups(string formula)
+        {
+            var fa = new FormulaAnalyzerExtension(formula);
+            return fa.ReturnLookups();
+        }
+    }
+
+
+    class App
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Felienne\Dropbox\TU Delft\Papers\2015\Link Paper\Lookups.csv");
+            string file = @"C:\Users\Felienne\Dropbox\TU Delft\Papers\2015\Link Paper\Lookups-result.csv";
+            File.WriteAllText(file, ""); //this empties the default file
+
+            string fileError = @"C:\Users\Felienne\Dropbox\TU Delft\Papers\2015\Link Paper\Lookups-errors.csv";
+            File.WriteAllText(fileError, ""); //this empties the errors file
+
+            foreach (string line in lines)
+            {
+                string formula = line.Substring(3);
+                formula = formula.Substring(0, formula.Length - 3);
+
+                try
+                {
+
+                    int numParam = LinkAnalysis.NumberOfParameters(formula);
+                    string type = "-";
+
+                    if (numParam == 4)
+                    {
+                        type = LinkAnalysis.TypeofFinalParameter(formula).ToString();
+                    }
+
+                    using (StreamWriter sw = File.AppendText(file))
+                    {
+                        sw.WriteLine("\"" + formula + "\"," + numParam + "," + type);
+                    }
+
+                    Console.WriteLine(type);
+                }
+                catch (Exception E)
+                {
+
+                    using (StreamWriter sw = File.AppendText(fileError))
+                    {
+                        sw.WriteLine("\"" + formula + "\"," + E.Message + "," + E.InnerException);
+                    }
+
+                }
+
+            }
+
+            Console.WriteLine("Links analyzed");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ZerudaTests/ZerudaTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zeruda;
+
+
+
+namespace ZerudaTests
+{
+    [TestClass]
+    public class ZerudaTests
+    {
+        [TestMethod]
+        public void VLOOKUP_True_Count()
+        {
+            string formula = "VLOOKUP(I8,[1]Sheet3!A$3:B$84,2,TRUE)";
+            int numParams = LinkAnalysis.NumberOfParameters(formula);
+            Assert.AreEqual(4, numParams);
+        }
+
+        [TestMethod]
+        public void Nested_VLOOKUP_Count()
+        {
+            string formula = "IF(VLOOKUP(I8,A5:B100,2,TRUE))";
+            int numParams = LinkAnalysis.NumberOfParameters(formula);
+            Assert.AreEqual(4, numParams);
+        }
+
+        [TestMethod]
+        public void Double_Nested_VLOOKUP_Count()
+        {
+            string formula = "IF(VLOOKUP(I8,A5:B100,2,TRUE),1,VLOOKUP(I8,A5:B100,3,FALSE))";
+            var allLookups = LinkAnalysis.ReturnLookups(formula);
+            Assert.AreEqual(2, allLookups.Count);
+        }
+
+
+        [TestMethod]
+        public void No_VLOOKUP_Count()
+        {
+            string formula = "SUM(A1:A17)";
+            var allLookups = LinkAnalysis.ReturnLookups(formula);
+            Assert.AreEqual(0, allLookups.Count);
+        }
+
+        [TestMethod]
+        public void One_VLOOKUP_Count()
+        {
+            string formula = "IF(VLOOKUP(I8,A5:B100,2,TRUE))";
+            int numParams = LinkAnalysis.NumberOfParameters(formula);
+            Assert.AreEqual(4, numParams);
+        }
+
+
+        [TestMethod]
+        public void VLOOKUP_Default()
+        {
+            string formula = "VLOOKUP(I8,[1]Sheet3!A$3:B$84,2)";
+            int numParams = LinkAnalysis.NumberOfParameters(formula);
+            Assert.AreEqual(3, numParams);
+        }
+
+        [TestMethod]
+        public void VLOOKUP_True_Type()
+        {
+            string formula = "VLOOKUP(I8,[1]Sheet3!A$3:B$84,2,TRUE)";
+            bool finalParam = LinkAnalysis.TypeofFinalParameter(formula);
+            Assert.AreEqual(true, finalParam);
+        }
+
+        [TestMethod]
+        public void VLOOKUP_False_Type()
+        {
+            string formula = "VLOOKUP(I8,[1]Sheet3!A$3:B$84,2,FALSE)";
+            bool finalParam = LinkAnalysis.TypeofFinalParameter(formula);
+            Assert.AreEqual(false, finalParam);
+        }
+
+        [TestMethod]
+        public void HLOOKUP_Three()
+        {
+            string formula = "HLOOKUP(Assumptions!$H$12,IDC!$I$40:$M$56,2+F42)";
+            int numParams = LinkAnalysis.NumberOfParameters(formula);
+            Assert.AreEqual(3, numParams);
+        }
+
+  
+    }
+}
